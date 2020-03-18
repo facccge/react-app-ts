1fb1d5c760c7269c128a412444ac3c58fbdee2a0
facccge <fcfc5702@qq.com>
2020-03-18 23:54:51 +0800

    Change content to own name.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const contentElement = getByText(/Cheng Fei - 费城/i);
+  expect(contentElement).toBeInTheDocument();
 });
